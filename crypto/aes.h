5cdb864a80da3d84deb1ddc6ec450679ff4faeb1
Josh Campbell <runguy@ymail.com>
2014-01-26 23:02:27 -0500

    added first version of aes encrypt/decrypt

--- crypto/aes.h
+++ crypto/aes.h
@@ -1,6 +1,8 @@
-#ifndef ESO_CENTRAL_CRYPTO_AES
-#define ESO_CENTRAL_CRYPTO_AES
+#ifndef ESO_CRYPTO_AES
+#define ESO_CRYPTO_AES
 
+#include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <openssl/rand.h>
 
 /*
@@ -28,6 +30,89 @@ unsigned char *get_new_AES_key(int size)
 
     // buf has now been initialized. 
     return buf;
+}
+
+/*
+ * This will encrypt *len bytes of data using AES-CBC mode.
+ * The return value has been malloc'd and must be freed after use.
+ * 
+ * @param size The size of the key in bits.
+ */
+unsigned char *aes_encrypt(unsigned char *key, unsigned char *plaintext, 
+        int *len, int size)
+{
+    EVP_CIPHER_CTX e;
+
+    EVP_CIPHER_CTX_init(&e);
+    switch (size)
+    {
+        case 128:
+            EVP_EncryptInit_ex(&e, EVP_aes_128_cbc(), NULL, key, NULL);
+            break;
+        case 256:
+            EVP_EncryptInit_ex(&e, EVP_aes_256_cbc(), NULL, key, NULL);
+            break;
+    }
+
+    // The max ciphertext length for n bytes of plaintext is 
+    // n + AES_BLOCK_SIZE - 1 bytes.
+    int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
+    unsigned char *ciphertext = (unsigned char *) malloc(c_len);
+
+    // TODO Allows reusing of 'e' for multiple encryption cycles
+    // however... we are only using it for one...
+    EVP_EncryptInit_ex(&e, NULL, NULL, NULL, NULL);
+
+    // Update ciphertext, c_len is filled with the length of ciphertext 
+    // generated, *len is the size of plaintext in bytes.
+    EVP_EncryptUpdate(&e, ciphertext, &c_len, plaintext, *len);
+
+    // update ciphertext with the final remaining bytes.
+    EVP_EncryptFinal_ex(&e, ciphertext+c_len, &f_len);
+
+    *len = c_len + f_len;
+
+    EVP_CIPHER_CTX_cleanup(&e);
+
+    return ciphertext;
+}
+
+/*
+ * This will decrypt *len bytes of ciphertext using AES-CBC mode.
+ * The return value has been malloc'd and must be freed after use.
+ *
+ * @param size The size of the key in bits.
+ */
+unsigned char *aes_decrypt(unsigned char *key, unsigned char *ciphertext, 
+        int *len, int size)
+{
+    EVP_CIPHER_CTX e;
+
+    EVP_CIPHER_CTX_init(&e);
+    switch (size)
+    {
+        case 128:
+            EVP_DecryptInit_ex(&e, EVP_aes_128_cbc(), NULL, key, NULL);
+            break;
+        case 256:
+            EVP_DecryptInit_ex(&e, EVP_aes_256_cbc(), NULL, key, NULL);
+            break;
+    }
+
+    // Because we have padding ON, we must allocate an extra cipher block size of memory.
+    int p_len = *len, f_len = 0;
+    unsigned char *plaintext = (unsigned char *) malloc(p_len + AES_BLOCK_SIZE);
+
+    // TODO Check these params.
+    EVP_DecryptInit_ex(&e, NULL, NULL, NULL, NULL);
+    EVP_DecryptUpdate(&e, plaintext, &p_len, ciphertext, *len);
+    EVP_DecryptFinal_ex(&e, plaintext+p_len, &f_len);
+
+    *len = p_len + f_len;
+
+    EVP_CIPHER_CTX_cleanup(&e);
+
+    return plaintext;
 }
 
 #endif
